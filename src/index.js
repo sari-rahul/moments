482fa1d5915495be8a451585892ab1da3ff6fe99
Sarimol Adampurath <sarisivadas93@gmail.com>
2024-04-07 12:05:34 +0000

    Add follow functionality to profiles

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CurrentUserProvider } from './contexts/CurrentUserContext';
+import { ProfileDataProvider } from './contexts/ProfileDataContext';
 
 ReactDOM.render(
   <React.StrictMode>
     <CurrentUserProvider>
+      <ProfileDataProvider>
     <Router>
       <App />
     </Router>
+    </ProfileDataProvider>
     </CurrentUserProvider>
 
   </React.StrictMode>,
